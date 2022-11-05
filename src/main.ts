9d4e7b7eeef6af04e2d13a91e785766637aa599a
Ehud Halamish <ehud@nymhealth.com>
2022-11-05 17:12:11 +0200

    copy todo to new journals

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import '@logseq/libs';
 
+import {BlockEntity} from '@logseq/libs/dist/LSPlugin';
+
+
+const todoRegex = /^(TODO|DONE)\s+/;
+const doneRegex = /^(DONE)\s+/;
+
 const settingsVersion = 'v1';
 export const defaultSettings = {
   keyBindings: {
@@ -40,21 +46,20 @@ const getSettings = (
   return key ? (merged[key] ? merged[key] : defaultValue) : merged;
 };
 
-const extractTodoState = (regex: RegExp, block: logseq.Block) => {
-  let todoMatch = regex.exec(block.content);
+const extractTodoState = (block: BlockEntity) => {
+  let todoMatch = todoRegex.exec(block.content);
   return (todoMatch !== null && todoMatch.length > 0) ? todoMatch[1] : '';
 };
 
 async function toggleTODO() {
-  let regex = /^(TODO|DONE)\s+/;
   const selected = await logseq.Editor.getSelectedBlocks();
   const blocks = (selected && selected.length > 1) ? selected : [await logseq.Editor.getCurrentBlock()];
-  const blocksInDifferentStates = (blocks.length > 0 && blocks.some(block => extractTodoState(regex, block) != extractTodoState(regex, blocks[0])))
+  const blocksInDifferentStates = (blocks.length > 0 && blocks.some(block => extractTodoState(block) != extractTodoState(blocks[0])));
   for (let block of blocks) {
     if (block?.uuid) {
-      let todoState = blocksInDifferentStates ? 'DONE' : extractTodoState(regex, block); // If blocks are in different states we "clear" them.
-      let strippedContent = regex.test(block.content)
-        ? block.content.replace(regex, '')
+      let todoState = blocksInDifferentStates ? 'DONE' : extractTodoState(block); // If blocks are in different states we "clear" them.
+      let strippedContent = todoRegex.test(block.content)
+        ? block.content.replace(todoRegex, '')
         : block.content;
       await logseq.Editor.updateBlock(
         block.uuid,
@@ -64,14 +69,135 @@ async function toggleTODO() {
   }
 }
 
+// const updateNewJournalWithAllTODOs = ({path, template}) => {
+//   // This hook is actually called on every route change, fortunately a route change happens when creating a new journal.
+//   console.log(path, template);
+//   debugger;
+// };
+
+async function queryCurrentRepoRangeJournals(untilDate) {
+  try {
+    const journals = await logseq.DB.datascriptQuery(`
+      [:find (pull ?p [*])
+       :where
+       [?b :block/page ?p]
+       [?p :block/journal? true]
+       [?p :block/journal-day ?d]
+       [(< ?d ${untilDate})]
+      ]
+    `);
+    return (journals || []).flat();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+
+async function updateNewJournalWithAllTODOs({blocks, txData, txMeta}) {
+  // This hook is actually called on every block update so checking if the block update is a journal creation transatcion should be fast.
+  let updatedAt = null;
+  let createdAt = null;
+  let isJournal = false;
+  for (let datum of txData) {
+    // datum structure https://tonsky.me/blog/datascript-internals/
+    if (datum[1] === 'updatedAt' && datum[4] === true) {
+      updatedAt = datum[2];
+    }
+    if (datum[1] === 'createdAt' && datum[4] === true) {
+      createdAt = datum[2];
+    }
+    if (datum[1] === 'journal?' && datum[4] === true) {
+      isJournal = datum[2];
+    }
+  }
+  const isJournalCreatedEvent = updatedAt !== null && updatedAt === createdAt && isJournal;
+  if (!isJournalCreatedEvent) {
+    return;
+  }
+
+  const newJournalBlock = blocks.find(block => block.hasOwnProperty('createdAt') && block['journal?'] === true);
+  console.log({newJournalBlock});
+
+  const prevJournals = await queryCurrentRepoRangeJournals(newJournalBlock['journalDay']);
+  console.log({prevJournals});
+  const latestJournal = prevJournals.reduce( // TODO: This aggregation should be handled by the query itself.
+    (prev, current) => prev['journal-day'] > current['journal-day'] ? prev : current
+  );
+  console.log({latestJournal});
+  const latestJournalBlocks = await logseq.Editor.getPageBlocksTree(latestJournal.name);
+  console.log({latestJournalBlocks});
+
+  let latestJournalBlockGroups = [[]];
+  for (let block of latestJournalBlocks) {
+    if (block.content !== '') {
+      latestJournalBlockGroups[latestJournalBlockGroups.length - 1].push(block);
+    } else {
+      latestJournalBlockGroups.push([]);
+    }
+  }
+  console.log({latestJournalBlockGroups});
+
+  let newJournalLastBlock = await getLastBlock(newJournalBlock.name);
+  for (let group of latestJournalBlockGroups) {
+    if (group.some(recursivelyCheckForTodoInBlock)) {
+      for (let block of group) {
+        newJournalLastBlock = await recursiveCopyBlocks(block, newJournalLastBlock);
+      }
+      newJournalLastBlock == await logseq.Editor.insertBlock(newJournalLastBlock.uuid, '');
+    }
+  }
+}
+
+async function recursiveCopyBlocks(srcBlock: BlockEntity, lastDestBlock: BlockEntity) {
+  // copied from https://github.com/vipzhicheng/logseq-plugin-move-block TODO add note in readme
+  if (doneRegex.test(srcBlock.content)) {
+    return lastDestBlock;
+  }
+  let newBlock = lastDestBlock;
+  console.log({srcBlock, lastDestBlock});
+  if (lastDestBlock.content !== '') {
+    newBlock = await logseq.Editor.insertBlock(lastDestBlock.uuid, srcBlock.content, {
+      sibling: true,
+    });
+  } else {
+    await logseq.Editor.updateBlock(lastDestBlock.uuid, srcBlock.content);
+    newBlock.content = srcBlock.content; // update doesn't update the instance.
+  }
+
+  if (srcBlock.children.length > 0) {
+    let newChildBlock = await logseq.Editor.insertBlock(newBlock.uuid, '');
+    for (let child of srcBlock.children) {
+      newChildBlock = await recursiveCopyBlocks(child, newChildBlock);
+    }
+  }
+  return newBlock;
+}
+
+
+function recursivelyCheckForTodoInBlock(block) {
+  return todoRegex.test(block.content) || block.children.some(recursivelyCheckForTodoInBlock);
+}
+
+export const getLastBlock = async function (
+  pageName: string
+): Promise<null | BlockEntity> {
+  // copied from https://github.com/vipzhicheng/logseq-plugin-move-block TODO add note in readme
+  const blocks = await logseq.Editor.getPageBlocksTree(pageName);
+  if (blocks.length === 0) {
+    return null;
+  }
+  return blocks[blocks.length - 1];
+};
+
+
 async function main() {
   initSettings();
   const keyBindings = getSettings('keyBindings', {});
 
   logseq.App.registerCommandPalette(
     {
-      key: `toggle-todo`,
-      label: `Toggle TODO for the current block`,
+      key: `toggle-todo-block`,
+      label: `Toggle TODO for the current block(s)`,
       keybinding: {
         mode: 'global',
         binding: keyBindings['TODO'] || 'mod+1',
@@ -81,6 +207,11 @@ async function main() {
       await toggleTODO();
     }
   );
+
+  logseq.DB.onChanged(async (params) => {
+    await updateNewJournalWithAllTODOs(params);
+  });
+
 }
 
 logseq.ready(main).catch(console.error);
